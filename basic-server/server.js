3705a8d65837e83750275b5d578394930b219ada
Unknown <nannerfox@me.com>
2018-11-26 19:09:46 -0600

    Added Basic Server

---
+++ basic-server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.set("port", process.env.PORT || 5000)
+
+app.get('/test', (req, res) => {
+    res.json([
+        {id:1, name: "this"},
+        {id:2, name: "is"},
+        {id:3, name: "a"},
+        {id:4, name: "test"}
+    ]);
+});
+
+app.listen(app.get("port"), () => {
+    console.log(
+      "Express started on http://localhost:" +
+        app.get("port") +
+        "; press Ctrl-C to terminate."
+    );
+  });
